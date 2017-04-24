3808c71eb9714b548e4de419fcd31ca27734f431
13xforever <ilya_veselov@kontur.ru>
2017-04-24 12:02:51 +0500

    No more inline scripts

--- inn_ogrn.js
+++ inn_ogrn.js
@@ -1,3 +1,5 @@
+'use strict'; // @ts-check
+
 function GetRandomNumber(str, length, force, validFirstDigits)
 {
 	var result = GetNumbersFromString("" + str);
@@ -238,3 +240,36 @@ function CheckButtons(input, inputButton, randomFunc, fixFunc, calcFunc, thresho
 		}
 	}
 }
+
+var innul;
+var innfl;
+var ogrnul;
+var ogrnfl;
+var okpo;
+var snils;
+var innulButton;
+var innflButton;
+var ogrnulButton;
+var ogrnflButton;
+var okpoButton;
+var snilsButton;
+
+document.addEventListener("DOMContentLoaded", function(){
+		innul = document.getElementById('innul');
+		innfl = document.getElementById('innfl');
+		ogrnul = document.getElementById('ogrnul');
+		ogrnfl = document.getElementById('ogrnfl');
+		okpo = document.getElementById('okpo');
+		snils = document.getElementById('snils');
+		innulButton = document.getElementById('innulbutton');
+		innflButton = document.getElementById('innflbutton');
+		ogrnulButton = document.getElementById('ogrnulbutton');
+		ogrnflButton = document.getElementById('ogrnflbutton');
+		okpoButton = document.getElementById('okpobutton');
+		snilsButton = document.getElementById('snilsbutton');
+
+		var input = document.getElementsByTagName('input');
+		for (var i = 0; i < input.length; i++)
+			if (input[i].type == 'text' && input[i].oninput)
+				input[i].oninput();
+});
